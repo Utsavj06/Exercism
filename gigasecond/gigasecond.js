731b100e1216bf66b0447acfb68f94b62999e013
utsav <utsav96jaiswal@gmail.com>
2021-09-29 12:07:08 +0530

    file add

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,7 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
+export const gigasecond = (date) => {
   //throw new Error('Remove this statement and implement this function');
-  
+  const gs = 1e9;  // 10^9 seconds
+// the below line get time of date paramenter.
+    console.log( date.getTime()+gs );
+
+//  first (new Date) create a object where parameter date is passed through and .getTime() select the time of date.
+   console.log(new Date(date.getTime() + gs *1000));
+  return (new Date(date.getTime() + gs *1000));
 };
